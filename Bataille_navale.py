6e99f2ab607a7d035c80137ec30b0435ef40157d
i182788 <i182788@MPS0919-02>
2020-03-03 14:42:57 +0100

    fichier bataille navale

---
+++ Bataille_navale.py
@@ -0,0 +1,24 @@
+import numpy as np
+#création du plateau
+class Plateau:
+    def __init__(self,ligne,colonne):
+        self.ligne= ligne
+        self.colonne= colonne
+        self.p = np.zeros((ligne,colonne),np.int32)
+ #convertion pour afficher la matrice
+    def __repr__(self):
+        return np.array_str(self.p)
+p=Plateau(10,10)
+print(p)
+#Placement bateau PorteAvion
+for k in range(5):
+    print("placez 1ere coordonée du PorteAvion (ligne,colonne)")
+    x1=int(input())
+    y1=int(input())
+    print("vous avez choisi la case : ",x1,y1)
+    """
+    Faire la classe placer_bateau : soit création case par case ou le bateau directement avec les paramètres x et y (premier point)
+    et direction (vertical ou horizontal)
+    """
+    p.placer_bateau(x1,y1)=5 #chaque type de bateau prend une valeur différente dans la matrice (si plus de 5 touché coulé)
+    print(p)
