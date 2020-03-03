7d0f03ccfcb85e1f5fc1b875b478735a9eb2da17
s176752 <s176752@MPS0919-01>
2020-03-03 14:56:05 +0100

    interface

---
+++ code_creation_interface.py
@@ -0,0 +1,41 @@
+import wx
+import os
+
+class MaFenetre(wx.Frame):    
+    def __init__(self):
+        chemin_script = os.path.dirname(os.path.realpath(__file__))
+        super().__init__(parent=None, title='Ma fenêtre avec BitmapButton')
+        self.bmp1 = wx.Bitmap("H:\semestre4\POO\PROJET\\Orange.png", wx.BITMAP_TYPE_PNG) 
+        self.bmp2 = wx.Bitmap("H:\semestre4\POO\PROJET\\Rouge.png", wx.BITMAP_TYPE_PNG) 
+        nb_boutons = 100
+        self.ma_grille = wx.GridSizer(rows=10, cols=10, vgap=0, hgap=0)
+        font = wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
+            wx.FONTWEIGHT_BOLD)
+        self.etat_bouton = None
+        for i in range(nb_boutons):
+            bouton = wx.BitmapButton(self, id=i, bitmap=self.bmp1)
+            bouton.SetFont(font)
+            bouton.Bind(wx.EVT_BUTTON, self.OnBouton)            
+            self.ma_grille.Add(bouton, i,wx.EXPAND)
+            if self.etat_bouton == None:
+                self.etat_bouton={i: False}
+            else:
+                self.etat_bouton[i] = False
+        self.SetSizerAndFit(self.ma_grille)        
+        self.Show()
+
+    def OnBouton(self, event):
+        if event.GetId() in self.etat_bouton:
+            bouton = event.GetEventObject()
+            if not self.etat_bouton[event.GetId()]:
+                self.etat_bouton[event.GetId()] = True
+                bouton.SetBitmapLabel(self.bmp2)
+            else:
+                self.etat_bouton[event.GetId()] = False
+                bouton.SetBitmapLabel(self.bmp1)
+
+       
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MaFenetre()
+    app.MainLoop()
